63a7e56dddef2e481371198b2c8c3d0b4aca4797
Lukas Friman <lukas@dreamplay.net>
2026-03-18 14:00:32 +0100

    Rename wildcard map modules.

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -1,11 +1,11 @@
 //! This module contains various collection types that can be used with the traits defined in this crate, such as [`WildcardHashMap`] and [`WildcardBTreeMap`].
 
 #[cfg(feature = "std")]
-mod hashmap;
+mod wildcard_hashmap;
 #[cfg(feature = "std")]
-pub use hashmap::WildcardHashMap;
+pub use wildcard_hashmap::WildcardHashMap;
 
 #[cfg(feature = "std")]
-mod btreemap;
+mod wildcard_btreemap;
 #[cfg(feature = "std")]
-pub use btreemap::WildcardBTreeMap;
+pub use wildcard_btreemap::WildcardBTreeMap;
